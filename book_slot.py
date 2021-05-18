389b1d4b97578784272fe096d001f7a5d7383e28
mayankagarwal2212 <mayankagarwal2212@gmail.com>
2021-05-19 01:34:50 +0530

    Update book_slot.py

--- book_slot.py
+++ book_slot.py
@@ -6,7 +6,10 @@ import json
 url = "https://cdn-api.co-vin.in/api/v2/appointment/schedule"
 
 # Get the session and center details from the available_slots.txt file
+center_id = 701792
 session_id = "0edb6bde-a57c-4b42-9dd4-85f177183c47"
+
+slot_time = "10:00AM-11:00AM"
 captcha = "7Rp2k"
 
 # beneficiary list for booking the slot. This can be fetched from the beneficiaries
@@ -15,11 +18,12 @@ beneficiaries = [
 ]
 # In case, aware of the center for booking and waiting for the session to open
 payload = json.dumps({
-  "center_id": 701792,
+  "center_id": center_id,
   "session_id": session_id,
-  "slot": "10:00AM-11:00AM",
+  "slot": slot_time,
   "captcha": captcha,
   "beneficiaries": beneficiaries,
+  # update this to 2 if its for the second dose
   "dose": 1
 })
 
